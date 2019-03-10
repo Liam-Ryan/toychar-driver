79129352fab35e9b20e10e9374c4bd35730b4126
Liam Ryan <liamryandev@gmail.com>
2019-03-10 18:49:09 +0000

    Added error handling for init and device cleanup for exit

--- character-driver-module.c
+++ character-driver-module.c
@@ -28,38 +28,69 @@ static struct file_operations fops = {
 	.write = toychar_write
 };
 
-static void init_device(struct cdev *dev, unsigned int minor) {
+static struct device *init_device(struct cdev *dev, unsigned int minor) {
 	dev_t cdev_id;
 
 	cdev_init(dev, &fops);
 	dev->owner = THIS_MODULE;
 	cdev_id = MKDEV(major, minor);
 	cdev_add(dev, cdev_id, 1);
-	device_create(toychar_class,
-			NULL,
-			cdev_id,
-			NULL,
-			TOYCHAR_DEV_NAME "%d", minor);	
-
+	return device_create(toychar_class,
+				NULL,
+				cdev_id,
+				NULL,
+				TOYCHAR_DEV_NAME "%d", minor);	
 }
 
 static int __init onload(void)
 {
 	int i;
+	struct device *curdev;
+	void *err;
+
 
 	alloc_chrdev_region(&dev_id, 0, TOYCHAR_DEVICES, TOYCHAR_DEV_NAME);
 	major = MAJOR(dev_id);
 	minor_start = MINOR(dev_id);
 	toychar_class = class_create(THIS_MODULE, TOYCHAR_CLASS);
-	for (i = 0; i < TOYCHAR_DEVICES; i++) {
-		init_device(&dev_list[i], minor_start + i);
+	
+	if (IS_ERR(toychar_class)) {
+		pr_err("Could not create class %s %d", __FUNCTION__, __LINE__);
+		err = toychar_class;
+		goto free_mem;
 	}
+
+	for (i = 0; i < TOYCHAR_DEVICES; i++) {
+		curdev = init_device(&dev_list[i], minor_start + i);
+		if (IS_ERR(curdev)) {
+			err = curdev;
+			pr_err("Could not create device %d %s %d", 
+					minor_start + i, __FUNCTION__, __LINE__);
+			goto destroy_devices;
+		}
+	}
+	
 	return 0;
+
+destroy_devices:
+	for (;i > -1; i--) {
+		device_destroy(toychar_class, MKDEV(major, minor_start + i));
+	}
+	class_destroy(toychar_class);
+	
+free_mem:
+	unregister_chrdev_region(dev_id, TOYCHAR_DEVICES);
+	return PTR_ERR(err);	
 }
 
 static void __exit onunload(void)
 {
-	
+	int i;
+	for (i = 0; i < TOYCHAR_DEVICES; i++) {
+		device_destroy(toychar_class, dev_list[i].dev); 
+	}
+	class_destroy(toychar_class);
+	unregister_chrdev_region(dev_id, TOYCHAR_DEVICES);
 }
 
 static int toychar_open(struct inode *node, struct file *filp)
@@ -84,5 +115,6 @@ static ssize_t toychar_write(struct file *filp, const char __user *data, size_t 
 
 module_init(onload);
 module_exit(onunload);
+MODULE_DESCRIPTION("Sample character driver which registers multiple devices");
 MODULE_AUTHOR("Liam Ryan <liamryandev@gmail.com>");
 MODULE_LICENSE("GPL");
